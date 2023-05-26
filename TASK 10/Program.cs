647c5a159cded77220199f8a32aaf90929cb0bfd
non sof <sofinalove2012@gmail.com>
2023-05-26 15:12:58 +0300

    delete 10

---
+++ TASK 10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+using System.Globalization;
+
+Console.WriteLine("take a three - digit num pls");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1>99 && 10000>num1)
+{
+    int result = num1 % 100;
+    int res = result / 10;
+    Console.WriteLine(res);
+}
+else
+{
+    Console.WriteLine("u wrote not a three -Digit num");
+}
